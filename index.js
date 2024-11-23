573901c1256ec4b649abf28cd13543f1eb1870df
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2024-11-23 15:16:40 +0700

    menambahkan route untuk bibit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import pupukRoute from './routes/pupuk.js';
+import bibitRoute from './routes/bibit.js';
 
 const app = express();
 const PORT = 8000;
 
 app.use(bodyParser.json());
+app.use('/pupuk', pupukRoute);
 app.use('/pupuk', pupukRoute);
 
 app.listen(PORT, () => 
