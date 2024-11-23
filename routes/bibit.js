8d69b8e2e9052f30096fdb480cae44575fe5ec69
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2024-11-23 15:37:52 +0700

    set up route untuk get /bibit

---
+++ routes/bibit.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const router = express.Router();
+
+const bibit = [
+    { nama: 'Padi', jenis: 'Unggul', harga: 20000 },
+    { nama: 'Jagung', jenis: 'Hibrida', harga: 15000 },
+    { nama: 'Kedelai', jenis: 'Lokal', harga: 18000 }
+];
+
+router.get('/', (req, res) => {
+    res.json(bibit);
+});
