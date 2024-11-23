0c89600c12ae23ccd117d241992fcae9aec0a80f
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2024-11-23 23:43:33 +0700

    mengganti variabel bibit menjadi let bibit = []

--- routes/bibit.js
+++ routes/bibit.js
@@ -1,11 +1,7 @@
 import express from 'express';
 const router = express.Router();
 
-const bibit = [
-    { nama: 'Padi', jenis: 'Unggul', harga: 20000 },
-    { nama: 'Jagung', jenis: 'Hibrida', harga: 15000 },
-    { nama: 'Kedelai', jenis: 'Lokal', harga: 18000 }
-];
+let bibit = [];
 
 router.get('/', (req, res) => {
     res.json(bibit);
