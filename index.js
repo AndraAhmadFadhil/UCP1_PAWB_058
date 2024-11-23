39a584a43daf208ea6db5de50c9d85aeae0a4a1b
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2024-11-23 15:17:55 +0700

    menambahkan route get untuk root/

--- index.js
+++ index.js
@@ -10,5 +10,10 @@ app.use(bodyParser.json());
 app.use('/pupuk', pupukRoute);
 app.use('/pupuk', pupukRoute);
 
+app.get('/', (req, res) => {
+    console.log(['GET ROUTE']);
+    res.send('selamat pagi');
+});
+
 app.listen(PORT, () => 
     console.log(`Server berjalan di port : http://localhost:${PORT}`));
