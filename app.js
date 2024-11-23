e22c3d654f479531de4ffd56a5216c5b3fd58c1c
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2024-11-23 15:25:06 +0700

    menambahkan ejs untuk view engine

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ const port = 3000;
 
 app.use(express.json()); 
 app.use('/todos', todoRoutes);
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 app.listen(port, () => console.log(`Server berjalan di port ${port}`));
