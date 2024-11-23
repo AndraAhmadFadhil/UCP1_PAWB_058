08bf211df968d5d741e7ee2fc0bcc3c41dac1fb3
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2024-11-23 15:40:03 +0700

    menyiapkan data pupuk

---
+++ routes/pupuk.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const router = express.Router();
+
+const pupuk = [
+    { nama: 'Urea', jenis: 'Kimia', harga: 100000 },
+    { nama: 'Kompos', jenis: 'Organik', harga: 50000 },
+    { nama: 'ZA', jenis: 'Kimia', harga: 75000 }
+];
