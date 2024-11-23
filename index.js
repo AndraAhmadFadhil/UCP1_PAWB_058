f20725e763a6821e8d5e401579db702eedc6d02b
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2024-11-23 15:15:57 +0700

    tambahkan route untuk pupuk

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import pupukRoute from './routes/pupuk.js';
 
 const app = express();
 const PORT = 8000;
 
 app.use(bodyParser.json());
+app.use('/pupuk', pupukRoute);
 
 app.listen(PORT, () => 
     console.log(`Server berjalan di port : http://localhost:${PORT}`));
