5c49d26771b977b5298867cf1e5c5938057fbea6
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2024-11-23 15:34:27 +0700

    set up route untuk put

--- routes/todo.js
+++ routes/todo.js
@@ -19,3 +19,15 @@ router.delete('/:id', (req, res) => {
     const deletedTodo = todos.splice(todoIndex, 1)[0];  
     res.status(200).json({ message: `Tugas '${deletedTodo.task}' telah dihapus` });
 });
+
+router.put('/:id', (req, res) => {
+    const todo = todos.find(t => t.id === parseInt(req.params.id));
+    if (!todo) return res.status(404).json({ message: 'Tugas tidak ditemukan' });
+
+    todo.task = req.body.task || todo.task;
+
+    res.status(200).json({
+        message: `Tugas dengan ID ${todo.id} telah diperbarui`,
+        updatedTodo: todo
+    });
+});
