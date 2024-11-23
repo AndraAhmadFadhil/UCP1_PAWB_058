1dcddae0f9f861e6fa49bddf52b45e7a52a740ee
AndraAhmadFadhil <andraahmadfadil@gmail.com>
2024-11-23 15:32:30 +0700

    set up route untuk post todos

--- routes/todo.js
+++ routes/todo.js
@@ -1,3 +1,13 @@
 router.get('/', (req, res) => {
     res.json(todos);
 });
+
+router.post('/', (req, res) => {
+    const newTodo = {
+        id: todos.length + 1,  
+        task: req.body.task,  
+        completed: false  
+    };
+    todos.push(newTodo);  
+    res.status(201).json(newTodo); 
+});
